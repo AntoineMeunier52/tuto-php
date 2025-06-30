650cba6de110cfe9472d482c022598c5df47d7de
MEUNIER Antoine <antoine.meunier@veibe.net>
2025-06-30 14:10:45 +0200

    initialization commit

---
+++ basic.php
@@ -0,0 +1,32 @@
+this is print by default.
+
+<?php
+//this is a comment on one line
+/*this is a comment on multiple */
+
+/*
+
+PHP => Hypertext Preprocessor
+
+-script language
+
+use for:
+  - server side scripting
+  - pragramming language in command line interface (ex: cron job)
+
+paradigme:
+  - prodedural
+  - OOP
+  - imperative
+
+Regex:
+  - compatible with Perl
+*/
+
+// echo: print a string
+// print: print a string
+echo "echo affiche sur le stdout\n";
+print "print affiche sur le stdout\n";
+//or
+print("print affiche sur le stdout\n");
+?>
---
+++ hello.php
@@ -0,0 +1,4 @@
+<?php
+echo "Hello, World!";
+phpinfo(); // A bunsh of information about the PHP environment
+?>
